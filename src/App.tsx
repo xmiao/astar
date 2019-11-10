10dfdda6def7ad836c4bff051fb45fd92a1c8a8d
Xijiang Miao <miaoxj@hotmail.com>
2019-11-10 16:34:18 +0800

    initial version.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ type Node = {
     distance: number; //f = g + h, so distance is g, which is parent's distance + distance to parent.
     x: number;
     y: number;
+    cost: number;
 };
 
 type ID = string;
@@ -21,19 +22,27 @@ class SourceMap {
         this.data = data;
     }
 
-    get(id: ID): Node {
+    get(id: ID): Node | null {
         const {nodeMap} = this;
         let nd = nodeMap[id];
         if (nd) return nd;
 
-        let [x, y] = id.split(",");
-        if (+x <= 0 || +y < 0 || +x >= MAX_WIDTH || +y <= MAX_HEIGHT) return {} as Node;
+        let [sx, sy] = id.split(",");
+        let [x, y] = [+sx, +sy];
+        if (x < 0 || y < 0 || x >= MAX_WIDTH || y >= MAX_HEIGHT) return null;
+        id = `${x},${y}`;
+        nd = nodeMap[id];
+        if (nd) return nd;
 
+        let base = (y * MAX_WIDTH + x) * 4;
+        let c = 0;
+        for (let i = 0; i < 3; i++) {
+            c += this.data[base + i];
+        }
         nd = nodeMap[id] = {
-            id,
-            x: +x,
-            y: +y,
+            id, x, y,
             parent: '',
+            cost: c > 0 ? Infinity : 1,
             distance: Infinity
         };
         return nd;
@@ -41,36 +50,22 @@ class SourceMap {
 
     neighbours(id: ID): ID[] {
         let nd = this.get(id);
-        let {id: id2} = nd;
-        if (!id2) return [];
+        if (!nd) return [];
 
         let {x, y} = nd;
-
-        return [[0, -1], [1, 0], [0, -1], [-1, 0]]
+        return [[0, -1], [1, 0], [0, 1], [-1, 0]]
             .map(([dx, dy]) => {
                 let tmpNode = this.get(`${x + dx},${y + dy}`);
+                if (!tmpNode) return '';
                 return tmpNode.id;
             })
             .filter(x => x);
-    }
-
-    cost(id: ID) {
-        let nd = this.get(id);
-        let {id: id2} = nd;
-        if (!id2) return [];
-        let {x, y} = nd;
-        let base = (x * MAX_HEIGHT + y) * 4;
-        let c = 0;
-        for (let i = 0; i < 3; i++) {
-            c += this.data[base + i];
-        }
-        if (c > 0) return Infinity;
-        // return c;
     }
 }
 
 function isEmpty(o: any) {
     if (!o) return true;
+    // noinspection LoopStatementThatDoesntLoopJS
     for (let k in o) return false;
     return true;
 }
@@ -81,21 +76,23 @@ class RobotMap extends Component {
     componentDidMount() {
         const ctx = this.mapCanvas.getContext('2d');
         let {width, height} = this.mapCanvas;
-        ctx.font = "bold 20px Arial";
+        ctx.font = "bold 20px ComicSans";
         ctx.fillStyle = 'rgb(200,0,0)';
         ctx.fillText("some some some", 20, 50);
         const {data} = ctx.getImageData(0, 0, width, height);
 
         let sourceMap = new SourceMap(data);
 
-        let curNode: Node = sourceMap.get("0,0");
+        let curNode: Node | null = sourceMap.get("0,0");
+        if (!curNode) return;
+
         let openList = {} as any;
         let closeList = {} as any;
         curNode.distance = 0;
         curNode.parent = "";
+        curNode.cost = 0;
 
         openList[curNode.id] = 1;
-
 
         while (!isEmpty(openList)) {
             //find minimum value from the openlist
@@ -105,21 +102,25 @@ class RobotMap extends Component {
             let shortDist = Infinity;
             let curID = '';
             for (let ndID in openList) {
+                if (!openList.hasOwnProperty(ndID)) continue;
                 let nd = sourceMap.get(ndID);
+                if (!nd) continue;
+
                 let {distance, x, y} = nd;
-                let cmpVal = distance + Math.abs(MAX_WIDTH - x) + Math.abs(MAX_HEIGHT - y);
-                if (shortDist < cmpVal) {
-                    shortDist = cmpVal;
+                let g = distance;
+                let h = Math.abs(MAX_WIDTH - x) + Math.abs(MAX_HEIGHT - y);
+                if (shortDist < g + h) {
+                    shortDist = g + h;
                     curID = ndID;
                 }
             }
             if (!curID) return;
+            delete openList[curID];
+            closeList[curID] = 1;
 
             //curID's neighbours go to the openlist.
-            const curNode2 = sourceMap.get(curID);
-            //distance means g.
-            const prt = sourceMap.get(curNode2.parent);
-            curNode2.distance = prt.distance + 1;
+            const curNode = sourceMap.get(curID);
+            if (!curNode) return;
 
             for (let id of sourceMap.neighbours(curID)) {
                 // if the node is processed, move on to next node.
@@ -127,8 +128,12 @@ class RobotMap extends Component {
 
                 openList.push(id);
                 const nd = sourceMap.get(id);
+                if (!nd) continue;
+
                 nd.parent = curID;
+                nd.distance = curNode.distance + nd.cost;
             }
+
         }
 
     }
